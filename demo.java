61672e5d39b6625e019af3eabd7946dbc7bea61e
jayanth <jayanthsuresh@gmail.com>
2022-06-07 18:02:48 +0000

    demo.java has been deleted by jayanth.

--- demo.java
+++
@@ -1,3 +0,0 @@
-
-hello world 
-here is git class
--- demo1.java
+++ demo1.java
@@ -1,7 +1,8 @@
 class demo1
 {
 	public static void main(string[]){
-	//comments on demo1.java
+	//comments on demo1.java and demo project 
+	//comment on java class
 		system.out.println("demo1");
 	}
 } 
