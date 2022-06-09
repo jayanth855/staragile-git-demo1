3e55b6ca65363a62569508facbf0922ec249c121
jayanth855 <jayanthsureshl1996@gmail.com>
2022-06-09 16:06:30 +0000

    commit updated

--- demo1.java
+++
@@ -1,8 +0,0 @@
-class demo1
-{
-	public static void main(string[]){
-	//comments on demo1.java and demo project 
-	//comment on java class
-		system.out.println("welcome to world of linux");
-	}
-} 
