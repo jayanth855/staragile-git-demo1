932d584497de51ef8795de1dd475b9390c5f66be
jayanth855 <jayanthsureshl1996@gmail.com>
2022-06-09 14:54:54 +0000

    file1 file2 file3 added

--- demo1.java
+++ demo1.java
@@ -3,6 +3,6 @@ class demo1
 	public static void main(string[]){
 	//comments on demo1.java and demo project 
 	//comment on java class
-		system.out.println("demo1");
+		system.out.println("welcome to world of linux");
 	}
 } 
